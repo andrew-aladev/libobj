281cad307f178263871d24ea10a79e326a3e39cf
mrzzzrm <Jingizu>
2012-12-25 18:14:52 +0100

    No message

--- include/obj.h
+++ include/obj.h
@@ -1,9 +1,6 @@
 #ifndef OBJ_H
 #define OBJ_H
 
-    typedef float[4] v_t;
-    typedef float[3] vn_t;
-    typedef float[3] vt_t;
 
     typedef struct {
         int v, vt, vn;
@@ -20,9 +17,9 @@
         unsigned int vt_size;
         unsigned int vn_size;
         f_t *f;
-        v_t *v;
-        vt_t *vt;
-        vn_t *vn;
+        float (*v)[4];
+        float (*vt)[3];
+        float (*vn)[3];
     } obj_t;
 
 
--- src/obj.c
+++ src/obj.c
@@ -4,40 +4,81 @@
 #include <string.h>
 #include <assert.h>
 #include <errno.h>
-#include <stderr.h>
+#include <limits.h>
 
 #define alen(a) (sizeof(a)/sizeof(*a))
 
-typedef struct {
-    char *str;
-    int size;
-} buf_t;
-
-
-static void push(void *ptr, void **a, void *size) {
-    *a = realloc(*a, ++size);
-    (*a)[size-1] = ptr;
+#define push(ptr, a, size) { \
+    a = realloc(a, (++size) * sizeof(*a)); \
+    memcpy(&a[size-1], ptr, sizeof(*a)); \
+    free(ptr); \
 }
 
-static void read_word(cur_t *c) {
+static int strsplit(const char *str, char d, char **segs, unsigned int *segs_size, unsigned int *slashes) {
+    unsigned int i;
+    unsigned int str_size = strlen(str);    
+    
+    *segs_size = 0;
+    *slashes = 0;
+    segs[*segs_size++] = &str[0];    
+    for(i = 0; i < str_size; i++) {
+        if(str[i] == d) {
+            *slashes++;
+            segs[*segs_size++] = &str[i+1]; 
+        }            
+    }
+    
+    return 0;
+}
 
+static void read_word(char *buf, unsigned int bufsize, const char *str) {    
+    unsigned int i;
+    unsigned int str_size = strlen(str);    
+    
+    for(;; str++) {
+        if(*str == '\0') {
+            buf[0] = '\0';    
+            return;
+        }
+        if(isprint(*str) && !isspace(*str)) 
+            break;       
+    }    
+    
+    for(i = 0; i < str_size-1 && i < bufsize-1; i++) {
+        if(isprint(str[i]) && !isspace(str[i])) 
+            buf[i] = str[i];        
+        else 
+            break;
+    }
+    buf[i] = '\0';
 }
 
 static int to_float(char *str, float *v) {
-
+    char *endptr;
+    *v = strtof(str, &endptr);
+    return endptr - str != strlen(str) ? -1 : 0;
 }
 
 static int to_int(char *str, int *v) {
-
+    long _v;
+    char *endptr;
+    _v = strtol(str, &endptr, 10);
+    if(endptr - str != strlen(str))
+        return -1;
+    if(_v < INT_MIN || _v > INT_MAX)
+        return -1;
+        
+    *v = (int)_v;
+    return 0;
 }
 
-static int read_floats(buf_t *b, float **buf, unsigned int size) {
+static int read_floats(const char *str, float **buf, unsigned int size) {
     unsigned int w;
 
     *buf = malloc(sizeof(**buf) * size);
     for(w = 0; w < size; w++) {
         char word[32];
-        read_word(word, b);
+        read_word(word, alen(word), str);
 
         if(strlen(word) == 0)
             break;
@@ -48,33 +89,32 @@ static int read_floats(buf_t *b, float **buf, unsigned int size) {
     return w;
 }
 
-static v_t *read_v(buf_t *b) {
-    v_t *v;
-
-    unsigned int size = read_floats(b, &v, alen(v));
+static int read_v(const char *str, float **v) {
+    *v = malloc(4 * sizeof(**v));
+    unsigned int size = read_floats(str, v, 4);
     assert(size >= 3);
-    v[3] = size >= 4 ? v[3] : 1.0;
+    (*v)[3] = size >= 4 ? (*v)[3] : 1.0;
 
-    return v;
+    return 0;
 }
 
-static vn_t *read_vn(buf_t *b) {
-    vn_t *vn;
+static int read_vn(const char *str, float **vn) {
+    *vn = malloc(3 * sizeof(**vn));
 
-    unsigned int size = read_floats(b, &vn, alen(vn));
+    unsigned int size = read_floats(str, vn, 3);
     assert(size >= 3);
 
-    return vn;
+    return 0;
 }
 
-static vt_t *read_vt(buf_t *b) {
-    vt_t *vt;
-
-    unsigned int size = read_floats(b, &vt, alen(vt));
+static int read_vt(const char *str, float **vt) {
+    *vt = malloc(3 * sizeof(**vt));
+    
+    unsigned int size = read_floats(str, vt, 3);
     assert(size >= 2);
-    vt[2] = size >= 3 ? vt[2] : 0.0;
+    (*vt)[2] = size >= 3 ? (*vt)[2] : 0.0;
 
-    return vt;
+    return 0;
 }
 
 static fv_t *read_fv(const char *str) {
@@ -82,13 +122,13 @@ static fv_t *read_fv(const char *str) {
     int slashes;
     fv_t _fv;
     fv_t *fv;
-    char **seg;
+    char *segs[3];
 
     fv = malloc(sizeof(*fv));
-    seg = strsplit(str, '/', &seg_size, &slashes);
+    strsplit(str, '/', segs, &seg_size, &slashes);
 
     assert(seg_size >= 1);
-    fv->v = to_int(seg[0]);
+    assert(!to_int(segs[0], &fv->v));
     fv->vt = -1;
     fv->vn = -1;
 
@@ -96,18 +136,18 @@ static fv_t *read_fv(const char *str) {
         case 0: break; // v
         case 1: // v/vt
             assert(seg_size >= 2);
-            assert(!to_int(seg[1], &fv->vt));
+            assert(!to_int(segs[1], &fv->vt));
         break;
         case 2:
             switch(seg_size) {
                 case 2: // v//vn
                     assert(seg_size >= 2);
-                    assert(!to_int(seg[1], &fv->vn));
+                    assert(!to_int(segs[1], &fv->vn));
                 break;
                 case 3:  // v/vt/vn
                     assert(seg_size >= 3);
-                    assert(!to_int(seg[1], &fv->vt));
-                    assert(!to_int(seg[2], &fv->vn));
+                    assert(!to_int(segs[1], &fv->vt));
+                    assert(!to_int(segs[2], &fv->vn));
                 break;
                 default:
                     assert(0);
@@ -120,7 +160,7 @@ static fv_t *read_fv(const char *str) {
     return fv;
 }
 
-static f_t *read_f(buf_t *b) {
+static f_t *read_f(const char *str) {
     f_t *f;
     unsigned int i;
 
@@ -130,15 +170,15 @@ static f_t *read_f(buf_t *b) {
 
     for(i = 0;; i++) {
         char word[32];
-        read_word(word, b);
-        if(strlen(word == 0))
+        read_word(word, alen(word), str);
+        if(strlen(word) == 0)
             break;
 
         fv_t *fv = read_fv(word);
         if(fv == NULL)
             continue;
 
-        push(fv, &f->fv, &f->fv_size);
+        push(fv, f->fv, f->fv_size);
     }
 
     return f;
@@ -148,33 +188,34 @@ static f_t *read_f(buf_t *b) {
 static void read_line(FILE *f, obj_t *obj) {
     char line[512];
     char s[16];
-    cur_t c = {line, alen(line)};
 
-    c = line;
     if(fgets(line, alen(line), f) == NULL) {
-        return NULL;
+        return;
     }
     assert(strlen(line) < alen(line) || feof(f));
-    read_word(s, alen(s), &c);
+    read_word(s, alen(s), line);
 
     if(strcmp(s, "#") == 0) {
         return;
     }
     else if(strcmp(s, "v") == 0) {
-        v_t *v = read_v(&c);
-        push(v, &obj->v, &obj->v_size);
+        float *v;
+        read_v(&line[1], &v);
+        push(v, obj->v, obj->v_size);
     }
     else if(strcmp(s, "vn") == 0) {
-        vn_t *vn = read_vn(&c);
-        push(v, &obj->vn, &obj->vn_size);
+        float *vn;
+        read_vn(&line[2], &vn);
+        push(vn, obj->vn, obj->vn_size);
     }
-    else if(strcmp(s, "vn") == 0) {
-        vn_t *vn = read_vn(&c);
-        push(v, &obj->vt, &obj->vt_size);
+    else if(strcmp(s, "vt") == 0) {
+        float *vt;
+        read_vt(&line[2], &vt);
+        push(vt, obj->vt, obj->vt_size);
     }
     else if(strcmp(s, "f") == 0) {
-        f_t *f = read_f(&c);
-        push(v, &obj->f, &obj->f_size);
+        f_t *f = read_f(&line[1]);
+        push(f, obj->f, obj->f_size);
     }
     else {
         assert(0);
